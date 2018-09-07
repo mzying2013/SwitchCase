d33b118aded551bd080d7f96bbf65e6d0684332a
Liu <liumin2011@gmail.com>
2018-09-07 11:18:42 +0800

    Initial Commit

---
+++ SwitchCase/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SwitchCase
+//
+//  Created by Liu on 2018/9/7.
+//  Copyright © 2018年 mzying.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
